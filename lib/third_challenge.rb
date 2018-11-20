904f18635dc1227a57fc2e67204b7b099dd6717c
Angie Goza <angielg9888@gmail.com>
2018-11-20 22:11:55 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,16 +14,14 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => [{
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
-        },
-        {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-        }]
+      :hero_friends => [
+        {:name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"},
+        {:name => "Mercutio",
+         :age => "18",
+         :attitude => "hot-headed"}
+        ]
    }, 
    :capulet => {
       :patriarch => {
@@ -39,19 +37,14 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => [{
-        :name => "Steven",
-        :age => "30",
-        :attitude => "confused"
-        },
-        {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"
-        }
+      :heroine_friends => [
+        {:name => "Steven",
+         :age => "30",
+         :attitude => "confused"},
+        {:name => "Nurse",
+         :age => "44",
+         :attitude => "worried"}
         ]
    }
   }
-
-  
 end
